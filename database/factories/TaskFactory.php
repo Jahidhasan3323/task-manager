fcd9ebec8e1feff6f1a2c5baae32cf9c5c33351a
Jahidhasan3323 <hasanj635@gmail.com>
2025-05-09 19:08:25 +0600

    write test case

--- database/factories/TaskFactory.php
+++ database/factories/TaskFactory.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Str;
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
  */
-class UserFactory extends Factory
+class TaskFactory extends Factory
 {
     /**
      * The current password being used by the factory.
@@ -24,11 +24,9 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
+            'title' => fake()->name(),
+            'sort_description' => fake()->name(),
+            'status' => rand(0,1),
         ];
     }
 
--- tests/Unit/TaskTest.php
+++ tests/Unit/TaskTest.php
@@ -1,116 +1,41 @@
 <?php
 
 use App\Models\Task;
-use App\Models\User;
-use Laravel\Sanctum\Sanctum;
-
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
 
-uses(RefreshDatabase::class);
-beforeEach(function () {
-    $this->baseUrl = 'api/tasks';
-    $this->user = User::factory()->create();
-    Sanctum::actingAs($this->user, ['*']);
+uses(TestCase::class, RefreshDatabase::class);
+
+it('can create a task', function () {
+
+    $task = Task::create([
+        'title' => 'Test Title',
+        'sort_description' => 'Test body content.',
+    ]);
+
+    expect($task->title)->toBe('Test Title')
+        ->and($task->sort_description)->toBe('Test body content.');
+
+    $this->assertDatabaseHas('tasks', [
+        'title' => 'Test Title',
+        'sort_description' => 'Test body content.',
+    ]);
 });
 
-it('tasks list column', function () {
-    Task::factory()->create();
-    $response = $this->getJson($this->baseUrl);
-    $response->assertOk()
-        ->assertJsonStructure([
-            'success',
-            'message',
-            'data' => [
-                'data' => [
-                    '*' => [
-                        'created_at',
-                        'deleted_at',
-                        'id',
-                        'sort_description',
-                        'status',
-                        'title',
-                        'updated_at'
-                    ]
-                ],
-                'links' => [
-                    'first',
-                    'last',
-                    'next',
-                    'prev'
-                ],
-                'meta' =>  [
-                    'current_page',
-                    'from',
-                    'last_page',
-                    'links',
-                    'path',
-                    'per_page',
-                    'to',
-                    'total',
-                ]
-            ]
-        ]);
-});
+it('can delete a task', function () {
 
-it('tasks list column missing', function () {
-    Task::factory()->create();
-    $response = $this->getJson($this->baseUrl);
-    $response->assertOk()
-        ->assertJsonMissing([
-            'success',
-            'message',
-            'data' => [
-                'data' => [
-                    '*' => [
-                        'created_at',
-                        'deleted_at',
-                        'id',
-                        'sort_description',
-                        'status',
-                        'updated_at'
-                    ]
-                ],
-            ]
-        ]);
-});
+    $task = Task::create([
+        'title' => 'Test Title for delete',
+        'sort_description' => 'Test body content for delete',
+    ]);
 
-it('create tasks', function () {
-    $payload = [
-      'title' => 'test task',
-      'sort_description' => 'test description'
-    ];
-    $response = $this->postJson($this->baseUrl, $payload);
+    $task->forceDelete();
 
-    $this->assertEquals(
-            $response['data']['title'],
-            $payload['title']
-        );
-});
-
-it('create tasks validation errors', function () {
-    $payload = [
-      'title' => '',
-      'sort_description' => 'test description'
-    ];
-    $response = $this->postJson($this->baseUrl, $payload);
-
-    $response->assertJsonValidationErrors('title');
-});
-
-it('delete task', function () {
-    $task = Task::factory()->create();
-
-    $response = $this->deleteJson("$this->baseUrl/$task->id");
-
-    $response->assertOk();
-});
-
-it('delete task failed', function () {
-    Task::factory()->create();
-
-    $response = $this->deleteJson($this->baseUrl . '/' . 2);
-
-    expect($response->status())->toBeIn([404]);
+    // Assert that the post no longer exists in the database
+    $this->assertDatabaseMissing('tasks', [
+        'title' => 'Test Title for delete',
+        'sort_description' => 'Test body content for delete',
+    ]);
 });
 
 
